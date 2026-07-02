8a97e773c449dc9dc07c3f0cb4812d2756f2a1e3
Mutou-star <766834265@qq.com>
2026-07-02 22:52:36 +0800

    Delete api directory

--- api/mood.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  res.status(200).json({ 
-    status: 'ok', 
-    message: 'Vercel function is running!',
-    timestamp: new Date().toISOString()
-  });
-}
